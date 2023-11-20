2a727ea69fc6010907284b8508463fc2409a7d1a
cmacdo1 <chrismacdonaldphotography@yahoo.com>
2023-11-20 10:29:07 -0700

    created api test route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -17,3 +18,6 @@ mongoose
         console.log('MongoDB Connection Successful')
     })
     .catch((error) => console.log(`${error} did not connect`));
+
+/* ROUTES */
+app.use('/api/user/', userRoutes);
