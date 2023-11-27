1583a1da275470ff654de08b7460cf9d373cf03a
cmacdo1 <chrismacdonaldphotography@yahoo.com>
2023-11-27 10:18:36 -0700

    added middleware and a function to handle errors

--- api/index.js
+++ api/index.js
@@ -22,5 +22,14 @@ mongoose
     .catch((error) => console.log(`${error} did not connect`));
 
 /* ROUTES */
-app.use('/api/user/', userRoutes);
+app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        message,
+        statusCode,
+    });
+});
